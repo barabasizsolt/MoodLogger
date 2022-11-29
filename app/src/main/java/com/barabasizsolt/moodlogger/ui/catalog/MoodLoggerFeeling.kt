6411e06665ec16d2763af7cf97dd1d88f7d6447f
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2022-11-29 23:05:11 +0200

    Remove unused imports.

--- app/src/main/java/com/barabasizsolt/moodlogger/ui/catalog/MoodLoggerFeeling.kt
+++ app/src/main/java/com/barabasizsolt/moodlogger/ui/catalog/MoodLoggerFeeling.kt
@@ -1,16 +1,10 @@
 package com.barabasizsolt.moodlogger.ui.catalog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -19,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
