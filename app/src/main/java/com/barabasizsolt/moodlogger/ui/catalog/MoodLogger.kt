8d4cf9db743adf2307bd0ad09ef69711ea1edd20
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2022-11-30 17:52:35 +0200

    Separate MoodLogger into MoodLoggerTitle & MoodLoggerBody.

--- app/src/main/java/com/barabasizsolt/moodlogger/ui/catalog/MoodLogger.kt
+++ app/src/main/java/com/barabasizsolt/moodlogger/ui/catalog/MoodLogger.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.barabasizsolt.moodlogger.data.Mood
 import com.barabasizsolt.moodlogger.ui.util.getHeaderTextStyle
 
 @Preview
@@ -34,43 +35,63 @@ fun MoodLogger(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = selectedMood.value.name,
-            style = radius.getHeaderTextStyle(),
-            color = MaterialTheme.colors.secondary,
-            fontWeight = FontWeight.Bold
+        MoodLoggerTitle(
+            title = selectedMood.value.name,
+            radius = radius
         )
 
-        Box {
-            MoodLoggerFrame(
-                content = {
-                    MoodLoggerFrameItems(
-                        currentSelectedMoodPosition = moods.indexOf(element = selectedMood.value),
-                        onClick = { position -> selectedMood.value = moods[position] },
-                        moodNumber = moods.size
-                    )
-                },
-                radius = radius,
-                modifier = Modifier.align(alignment = Alignment.Center)
-            )
-
-            MoodLoggerArrows(
-                content = { MoodLoggerArrowItems(radius = radius) },
-                radius = radius,
-                modifier = Modifier.align(alignment = Alignment.Center)
-            )
-
-            MoodLoggerFeelings(
-                radius = radius,
-                modifier = Modifier.align(alignment = Alignment.Center)
-            )
-
-            Mood(
-                radius = radius,
-                selectedMood = selectedMood.value,
-                modifier = Modifier.align(alignment = Alignment.Center)
-            )
-        }
+        MoodLoggerBody(
+            mood = selectedMood.value,
+            onMoodChange = { mood -> selectedMood.value = mood },
+            radius = radius
+        )
     }
 }
+
+@Composable
+private fun MoodLoggerTitle(title: String, radius: Dp) = Text(
+    text = title,
+    style = radius.getHeaderTextStyle(),
+    color = MaterialTheme.colors.secondary,
+    fontWeight = FontWeight.Bold
+)
+
+@Composable
+private fun MoodLoggerBody(
+    modifier: Modifier = Modifier,
+    mood: Mood,
+    onMoodChange: (mood: Mood) -> Unit,
+    radius: Dp
+) = Box(modifier = modifier) {
+    MoodLoggerFrame(
+        content = {
+            MoodLoggerFrameItems(
+                currentSelectedMoodPosition = moods.indexOf(element = mood),
+                onClick = { position -> onMoodChange(moods[position]) },
+                moodNumber = moods.size
+            )
+        },
+        radius = radius,
+        modifier = Modifier.align(alignment = Alignment.Center)
+    )
+
+    MoodLoggerArrows(
+        content = { MoodLoggerArrowItems(radius = radius) },
+        radius = radius,
+        modifier = Modifier.align(alignment = Alignment.Center)
+    )
+
+    MoodLoggerFeelings(
+        radius = radius,
+        modifier = Modifier.align(alignment = Alignment.Center)
+    )
+
+    MoodEmoji(
+        radius = radius,
+        selectedMood = mood,
+        modifier = Modifier.align(alignment = Alignment.Center)
+    )
+}
+
+private val moods = Mood.getMoods()
 
